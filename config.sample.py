0a1d4733d55e1ffd803494b1f5a2049eeffad8a5
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-04-01 12:18:05 +0530

    Delete config.sample.py

--- config.sample.py
+++
@@ -1,7 +0,0 @@
-DRIVER_PATH = r"chromedriver.exe" #path of chromedriver
-USERNAME = r"userid"
-PASSWORD = r"password"
-URL = r"https://myacademy.oracle.com/lmt/xlr8login.login?site=oa" #login page url
-
-TIMEOUT = 1
-READ_TIME = 5
